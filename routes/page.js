e7fa2dab09a139435b02311c7b820deaa295c097
mageunwon <geunwon1947@gmail.com>
2023-03-03 16:04:54 +0900

    팔로우 팔로잉 기능

--- routes/page.js
+++ routes/page.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const {Post, User, Hashtag} = require('../models');
+const { hash } = require('bcrypt');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
   res.locals.user = req.user; // 넌적스에서 user 객체를 통해 사용자 정보에 접근할 수 있게 되었음.
-  res.locals.followerCount = 0;
-  res.locals.followingCount = 0;
-  res.locals.followerIdList = [];
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
   next();
 });
 
@@ -26,5 +28,48 @@ router.get('/', (req, res, next) => {
     twits,
   });
 });
+
+router.get('/', async(req, res, next)=>{
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order:[['createdAt', 'DESC']],
+    });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.get('/hashtag', async (req, res, next)=>{
+  const query = req.query.hashtag;
+  if(!query) {
+    return res.redirect('/');
+  }
+  try {
+    const hashtag = await Hashtag.findOne({where: {title: query}});
+    let posts = [];
+    if(hash) {
+      posts = await hashtag.getPosts({include: [{model: User}]});
+    }
+
+    return res.render('main', {
+      title: `{query} | NodeBird`,
+      twits: posts,
+    });
+  } catch(error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+module.exports = router;
 
 module.exports = router;
